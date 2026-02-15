887601a6539d6fdf6b217214cba87f3e4ffff4f0
Noelia Navas <noelianavasgarrido96@gmail.com>
2026-02-15 23:13:16 +0100

    .....

---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB-QHNYVb0ZDd-6aBt7MTsNnGasH-vRCRw",
+    authDomain: "proyectoangular002-17287.firebaseapp.com",
+    projectId: "proyectoangular002-17287",
+    storageBucket: "proyectoangular002-17287.firebasestorage.app",
+    messagingSenderId: "616744157328",
+    appId: "1:616744157328:web:11576cb0832c5fd04c5815"
+  }
+};
